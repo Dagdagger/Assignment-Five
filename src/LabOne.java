6653473af7249cb89b24e9722c7b3d403da0a2d2
Dagdagger <omar_brt@yahoo.com>
2015-09-26 05:18:15 -0700

    LabOne clean change
    Added:
    COMMENTS
    CREATE J-DIALOG METHOD:
    * method to clean up repeated code
    * generates a custom JDialog and 
    * Message Dialog depending on args
    ACTION PERFORMED AND GET ACTION COMMAND:
    * Action codes are a cleaner way to represent ActionEvents
    * the appropriate string generates the desired action
    CHANGED VARIABLE SCOPE
    * The Images and JLabels were once initiated in the
    * addComponents method, now they are initiated outside 
    * of it in order to allow the getActionCommand method
    * to use the desired JLabels

--- src/LabOne.java
+++ src/LabOne.java
@@ -1,11 +1,10 @@
-
 import java.awt.*;
 
 import javax.swing.*;
- 
+
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-public class LabOne extends JFrame {
+public class LabOne extends JFrame implements ActionListener {
 	
 	private static final long serialVersionUID = -5230739004178973444L;
 	final static int maxGap = 300;
@@ -16,15 +15,18 @@ public class LabOne extends JFrame {
         super(name);
         setResizable(true);
     }
-  
-    ImageIcon one = createImageIcon("duke09.gif");
-    ImageIcon two = createImageIcon("duke11.gif");
-    ImageIcon three = createImageIcon("duke03.gif");
-    ImageIcon four = createImageIcon("duke04.gif");
-    ImageIcon hello = createImageIcon("hello.gif");
-
-
-protected static ImageIcon createImageIcon(String path) {
+    
+    
+    
+    /* Image Creation methods:
+     * sets the source images to ImageIcons 
+     * then turns them into a JLabel object so that it can
+     * be used by the JDialog method
+     */
+    
+    
+    // Gets source file and creates ImageIcon object
+    protected static ImageIcon createImageIcon(String path) {
         java.net.URL imgURL = LabOne.class.getResource(path);
         if (imgURL != null) {
             return new ImageIcon(imgURL);
@@ -33,6 +35,24 @@ protected static ImageIcon createImageIcon(String path) {
             return null;
         }
     }
+   
+  // Making ImageIcon objects...
+    ImageIcon one = createImageIcon("duke09.gif");
+    ImageIcon two = createImageIcon("duke11.gif");
+    ImageIcon three = createImageIcon("duke03.gif");
+    ImageIcon four = createImageIcon("duke04.gif");
+    ImageIcon hello = createImageIcon("hello.gif");
+    ImageIcon vanilla = createImageIcon("vanilla.gif");
+    ImageIcon clown = createImageIcon("clown.gif");
+    ImageIcon diploma = createImageIcon("diploma.gif");
+    
+    
+  /*ImageIcons are turned into JLabelObjects to be used by
+   * the JDialogs */
+    JLabel salutation = new JLabel(hello);
+    JLabel coffee = new JLabel(vanilla);
+    JLabel congrats = new JLabel(clown);
+    JLabel graduate = new JLabel(diploma);
    
 
     
@@ -43,106 +63,87 @@ protected static ImageIcon createImageIcon(String path) {
         JPanel controls = new JPanel();
         controls.setLayout(new GridLayout(2,3));
         
+        
         JButton b = new JButton("Just fake button");
         Dimension buttonSize = b.getPreferredSize();
         dukeGrid.setPreferredSize(new Dimension((int)(buttonSize.getWidth() * 2.5)+maxGap,
                 (int)(buttonSize.getHeight() * 3.5)+maxGap * 2));
+       
         
-        ImageIcon one = createImageIcon("duke09.gif");
-        ImageIcon two = createImageIcon("duke11.gif");
-        ImageIcon three = createImageIcon("duke03.gif");
-        ImageIcon four = createImageIcon("duke04.gif");
-        ImageIcon hello = createImageIcon("hello.gif");
-        ImageIcon vanilla = createImageIcon("vanilla.gif");
-        ImageIcon clown = createImageIcon("clown.gif");
-        ImageIcon diploma = createImageIcon("diploma.gif");
-        
-        
-        
-        final JLabel salutation = new JLabel(hello);
-        final JLabel coffee = new JLabel(vanilla);
-        final JLabel congrats = new JLabel(clown);
-        final JLabel graduate = new JLabel(diploma);
-        
+
+        /* Creates and adds button objects with custom Action Command Code and
+        * listeners to generate the appropriate dialog and image
+        */
         b1 = new JButton(null, one);
-        b1.addActionListener(new ActionListener()
-        {
-        	public void actionPerformed(ActionEvent e)
-            {
-                JDialog dialog = new JDialog ();
-                dialog.setSize(567, 378);
-                dialog.setTitle("Just Duke saying Hi!");
-                dialog.getContentPane().add(salutation);
-                dialog.setVisible(true);
-                JOptionPane.showMessageDialog(null, "Just Duke Saying Hi!");
-            }
-        });
+        b1.addActionListener(this);
+        b1.setActionCommand("salutations");
+        dukeGrid.add(b1);
  
         
         b2 = new JButton(null, two);
-        b2.addActionListener(new ActionListener()
-        {
-        	public void actionPerformed(ActionEvent e)
-            {
-        		 JDialog dialog = new JDialog ();
-                 dialog.setSize(351, 342);
-                 dialog.setTitle("Congrats!");
-                 dialog.getContentPane().add(congrats);
-                 dialog.setVisible(true);
-        		JOptionPane.showMessageDialog(null, "Duke would like to thank you for for an explosive \n job well done!", "Thanks!!", JOptionPane.INFORMATION_MESSAGE);
-            }
-        });
+        b2.addActionListener(this);
+        b2.setActionCommand("congratulations");
+        dukeGrid.add(b2);
+       
         
         b3 = new JButton(null, three);
-        b3.addActionListener(new ActionListener()
-        {
-        	public void actionPerformed(ActionEvent e)
-            {
-                JDialog dialog = new JDialog ();
-                dialog.setSize(305, 363);
-                dialog.setTitle("Just Duke saying Hi!");
-                dialog.getContentPane().add(coffee);
-                dialog.setVisible(true);
-               JOptionPane.showMessageDialog(null, "Duke sure likes his cup of java", "Coffee!!", JOptionPane.INFORMATION_MESSAGE);
-            }
-        });
+        b3.addActionListener(this);
+        b3.setActionCommand("coffee");
+        dukeGrid.add(b3);
         
         b4 = new JButton(null, four);
-        b4.addActionListener(new ActionListener()
-        {
-        	public void actionPerformed(ActionEvent e)
-            {
-                JDialog dialog = new JDialog ();
-                dialog.setSize(588, 392);
-                dialog.setTitle("You got your Java Degree");
-                dialog.getContentPane().add(graduate);
-                dialog.setVisible(true);
-               JOptionPane.showMessageDialog(null, "Duke has graduated from the school of java!", "yay Java degree", JOptionPane.INFORMATION_MESSAGE);
-            }
-        });
-        
-        
-        
-        
-        //Add buttons to experiment with Grid Layout
-        dukeGrid.add(b1);
-        dukeGrid.add(b2);
-        dukeGrid.add(b3);
+        b4.setActionCommand("diploma");
+        b4.addActionListener(this);
         dukeGrid.add(b4);
-
-   
         
-     ;
+
+        // Borders and Separators       
+
         pane.add(dukeGrid, BorderLayout.NORTH);
         pane.add(new JSeparator(), BorderLayout.CENTER);
         pane.add(controls, BorderLayout.SOUTH);
     }
- 
-    /**
-     * Create the GUI and show it.  For thread safety,
-     * this method is invoked from the
-     * event dispatch thread.
+    
+    // single method for creating a custom JDialog and a showMessageDialogue
+    
+    public void createJDialog ( int width, int height, String Title, JLabel labelImage, String message, String windowTitle){
+    	
+    	 JDialog dialog = new JDialog ();
+         dialog.setSize(height, width);
+         dialog.setTitle(Title);
+         dialog.getContentPane().add(labelImage);
+         dialog.setVisible(true);
+        JOptionPane.showMessageDialog(null, message, windowTitle, JOptionPane.INFORMATION_MESSAGE);
+    	
+    }
+    
+    
+    /* Sets a custom JDialog depending on the action command
+     * the action command determines the JDialog that is generated
      */
+    
+    public void actionPerformed(ActionEvent e) {
+    	if ("diploma".equals(e.getActionCommand())){
+			createJDialog (588, 392, "You got your Java degree!", graduate , "Duke has Graduated from the school of java", "Yay Java Degree");
+    		}
+    	if("congratulations".equals(e.getActionCommand())){
+    		createJDialog(351, 342, "Congrats!", congrats,"Duke would like to thank you for an explosive \n job well done!", "Thanks!!");
+    	}
+    	if("salutations".equals(e.getActionCommand())){
+    		createJDialog(567, 378, "Just Duke saying Hi!", salutation, "Just Duke saying Hi!", "Hello!");
+    	}
+    	if("coffee".equals(e.getActionCommand())){
+    		createJDialog(305, 363, "Duke Swimming in Coffee", coffee, "Duke sure likes his cup of java!", "Coffee!");
+    	}
+
+    }
+ 
+ /* main method and GUI creation obtained from Java GridLayout Examples
+  * in oracle tutorial web page to show a nice and proper GUI
+  * source: https://docs.oracle.com/javase/tutorial/uiswing/layout/grid.html
+  */
+    
+    
     private static void createAndShowGUI() {
         //Create and set up the window.
         LabOne frame = new LabOne("GridLayout");
@@ -153,6 +154,9 @@ protected static ImageIcon createImageIcon(String path) {
         frame.pack();
         frame.setVisible(true);
     }
+    
+    
+   
     
     public static void main(String[] args) {
         /* Use an appropriate Look and Feel */
